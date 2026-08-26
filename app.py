cf22cab6968d23cf5225c71b2bbb531a9cb0bdc7
Bscoble <51501360+Bscoble@users.noreply.github.com>
2026-08-26 06:41:33 +0000

    varous

--- app.py
+++ app.py
@@ -1489,7 +1489,7 @@ else:
                                         ):
                                             res["is_favorite"] = new_favorite_state
                                             if new_favorite_state:
-                                                st.success("Added to favourites")
+                                                st.toast("Added to favourites", icon="♥")
                                 with add_column:
                                     if st.button("➕ Add", key=f"add_search_{idx}"):
                                         matched_qty, matched_unit = infer_quantity_and_unit(res["title"])
